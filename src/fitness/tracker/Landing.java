9c34a55fdcab626bb1f5f217355084499316d9b1
Harshit Raghuvanshi <rharshit98@gmail.com>
2018-10-06 20:46:09 +0530

    Change Theme to Windows

--- src/fitness/tracker/Landing.java
+++ src/fitness/tracker/Landing.java
@@ -6,6 +6,10 @@
 package fitness.tracker;
 
 import fitness.tracker.pages.Login;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 /**
  *
@@ -17,7 +21,18 @@ public class Landing {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+            // TODO code application logic here
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Landing.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            Logger.getLogger(Landing.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(Landing.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(Landing.class.getName()).log(Level.SEVERE, null, ex);
+        }
         new Login().show();
     }
 }
